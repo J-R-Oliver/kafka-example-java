325e6bd80aca36901ea03bdd116a3967c39404f3
James Oliver <james.oliver@engineering.digital.dwp.gov.uk>
2022-10-27 14:33:41 +0100

    feat: creates hello world application

---
+++ src/main/java/dev/jamesoliver/kafka/Main.java
@@ -0,0 +1,13 @@
+package dev.jamesoliver.kafka;
+
+/**
+ * Main class for Hello World application.
+ */
+public class Main {
+
+  private Main() {}
+
+  public static void main(String[] args) {
+    System.out.print("Hello World");
+  }
+}
---
+++ src/test/java/dev/jamesoliver/kafka/MainTest.java
@@ -0,0 +1,32 @@
+package dev.jamesoliver.kafka;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+  private final PrintStream stdout = System.out;
+  private final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+  @BeforeEach
+  void beforeEach() {
+    System.setOut(new PrintStream(byteArrayOutputStream));
+  }
+
+  @AfterEach
+  void afterEach() {
+    System.setOut(stdout);
+  }
+
+  @Test
+  void mainTest() {
+    Main.main(new String[] {});
+    assertEquals("Hello World", byteArrayOutputStream.toString());
+  }
+}
